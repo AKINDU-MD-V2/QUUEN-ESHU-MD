460952e50ba081931b0dc4fd4c41acb533c05791
CYBER-ESHU-2006 <eshaninduvaraachala@gmail.com>
2025-06-30 16:53:46 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "ESHU-MD=Ia8HCbJR#2PmwQqGUqW_hHHA6wHKc82pgSqA9C4MDufZMmnxBC9k",
+SESSION_ID: process.env.SESSION_ID || "ESHU-MD=TJV1mCBK#MJ00gsDEkAc2HwVDjk1DN_RN56q0r7KHppWfZD-EtP8",
 MONGODB: process.env.MONGODB || "mongodb+srv://mohsin:mohsin@cluster0.iauaztt.mongodb.net/?retryWrites=true&w=majority", //add your mongodb url        
 MODE: process.env.MODE || "private",  // Add Your Bot Mode (groups/privet/public)
 PREFIX: process.env.PREFIX || ".",   // Add Your Custom Prefix 
