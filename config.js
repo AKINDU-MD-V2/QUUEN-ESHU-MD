f739f2c0ef165e15391444fb4fd40ef620e4ce23
AKINDU-MD-V2 <dimanshaakindu07@gmail.com>
2025-07-19 07:49:53 +0530

    Update config.js

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ SESSION_ID: process.env.SESSION_ID || "Gd00DByC#Fz-5TI7PpzGELwat6_8AIJHn9NR4LuNe
 MONGODB: process.env.MONGODB || "mongodb+srv://mohsin:mohsin@cluster0.iauaztt.mongodb.net/?retryWrites=true&w=majority", //add your mongodb url        
 MODE: process.env.MODE || "private",  // Add Your Bot Mode (groups/privet/public)
 PREFIX: process.env.PREFIX || ".",   // Add Your Custom Prefix 
-OWNER_REACT: process.env.OWNER_REACT || "true", // true or false
+OWNER_REACT: process.env.OWNER_REACT || "false", // true or false
 BOT_NAME: process.env.BOT_NAME || "AKINDU-MD",
 AUTO_VOICE: process.env.AUTO_VOICE || "true", // true or false
 AUTO_STICKER: process.env.AUTO_STICKER || "true", // true or false
@@ -24,7 +24,7 @@ AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true", // true or false
 FILENAME: process.env.FILENAME || "QUEEN-ESHU-MD-V1",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/xmhW7DJ/6252.png", 
 CHANNEL_NAME: process.env.CHANNEL_NAME || "AKINDU-MD",
-NEWSLETTER_ID: process.env.NEWSLETTER_ID || "120363303242803015@newsletter",
+NEWSLETTER_ID: process.env.NEWSLETTER_ID || "",
 TITLE: process.env.TITLE || "AKINDU-MD",
 BODY: process.env.BODY || "CREATED BY ESHAN TECH SL🛡️",
 WEBURL: process.env.WEBURL|| "https://github.com/CYBER-ESHU/ESHU-MD",
